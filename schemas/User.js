612c67068ebf3387058b26c3ead7861c31a56ab7
shashwat <shashwat0313@yahoo.co.in>
2024-05-16 18:02:29 +0530

    setting back to NO encryption, again

--- schemas/User.js
+++ schemas/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const listSchema = require("./List")
 
-const mongooseEncryption = require('mongoose-encryption')
-const db_enc_key = Uint8Array.prototype.slice.call(Buffer.from(process.env.DB_ENCKEY), 0,32)
-const db_signing_key = Uint8Array.prototype.slice.call(Buffer.from(process.env.DB_SIGNING_KEY), 0,64)
+// const mongooseEncryption = require('mongoose-encryption')
+// const db_enc_key = Uint8Array.prototype.slice.call(Buffer.from(process.env.DB_ENCKEY), 0,32)
+// const db_signing_key = Uint8Array.prototype.slice.call(Buffer.from(process.env.DB_SIGNING_KEY), 0,64)
 
 const userSchema = new mongoose.Schema(
     {
@@ -16,7 +16,6 @@ const userSchema = new mongoose.Schema(
     }
 )
 
-
-userSchema.plugin(mongooseEncryption, {encryptionKey:db_enc_key,signingKey:db_signing_key, excludeFromEncryption:['_id', '__v']})
+// userSchema.plugin(mongooseEncryption, {encryptionKey:db_enc_key,signingKey:db_signing_key, excludeFromEncryption:['_id', '__v']})
 
 module.exports =  userSchema
